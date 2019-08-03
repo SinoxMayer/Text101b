73512aad337f00b48ab1943bdec9cacfe4f5f78c
SinoxMayer <arda_kaptan@hotmail.com>
2019-08-03 10:56:18 +0300

    ek testler
    ek testler denemeler

---
+++ Assets/FightState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "FightState")]
+public class FightState : ScriptableObject
+
+{
+
+    [TextArea(1, 1)] [SerializeField] string hpOfMonster;
+    [TextArea(1, 1)] [SerializeField] string dpsOfMonster;
+    [TextArea(1, 1)] [SerializeField] string armorOfMonster;
+
+    public string   GetStateFight()
+    {
+
+        return hpOfMonster ;
+    }
+}
